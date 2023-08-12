eb29343b738ea0c27a34c37b9fd37c4cc5ba3f53
eggmun98 <eggmun98@gmail.com>
2023-08-12 17:19:00 +0900

    [2023.08.12] Docs 불필요한 코드 삭제

--- routes/form.js
+++ routes/form.js
@@ -2,7 +2,7 @@ const express = require("express"); // express 인스턴스 사용한다는 뜻
 const router = express.Router(); // express 라우터를 사용하기 위해 선언
 
 router.get("/", (req, res, next) => {
-  res.render("form", { title: "Form 테스트" });
+  res.render("form");
 });
 
 router.post("/", (req, res, next) => {
